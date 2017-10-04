e55567041e4220c32dff83b8cd18de3d1abb6276
Bethany Priode <bpriode@gmail.com>
2017-10-03 22:43:11 -0400

    wrong answers correctly adding to attempts

--- public/main.js
+++ public/main.js
@@ -1,9 +1,11 @@
 let correctButton = document.getElementsByClassName('correct')
+let incorrectButton = document.getElementsByClassName('incorrect')
 let flashCards = document.getElementsByClassName('flashCards')
 let scoreDisplay = document.getElementById('totalScore')
 
 
 let score = 0
+let attempt = 0
 let total = flashCards.length
 
 console.log('total', total);
@@ -13,12 +15,25 @@ console.log('score',score);
 for (var i = 0; i < correctButton.length; i++) {
   correctButton[i].addEventListener('click', function() {
       score++
+      attempt++
       console.log(correctButton.length);
       console.log(score);
       scoreDisplay.innerHTML = `Your Score: ${score}/${total}`
-  if(score === total) {
-    console.log('Gameover');
-    window.location.href = "/quizOver/"
+        if(attempt === total) {
+          console.log('Gameover');
+          window.location.href = "/quizOver/";
   }
  })
+}
+
+for (var i = 0; i < incorrectButton.length; i++) {
+  incorrectButton[i].addEventListener('click', function() {
+    attempt++
+    scoreDisplay.innerHTML = `Your Score: ${score}/${total}`
+    console.log('attempt', attempt);
+        if(attempt === total) {
+          console.log('Gameover');
+          window.location.href = "/quizOver/";
+    }
+  })
 }
